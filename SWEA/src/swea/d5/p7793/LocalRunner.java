c41d554a255799329a139a5e52a52ff44371c3c1
BaeHyungJoon <bhj01073206033@gmail.com>
2026-08-12 17:18:24 +0900

    SWEA p7793 오 나의 여신님!

--- SWEA/src/swea/d5/p7793/LocalRunner.java
+++ SWEA/src/swea/d5/p7793/LocalRunner.java
@@ -1,10 +1,11 @@
 package swea.d5.p7793;
 
+import java.io.FileInputStream;
+
 public class LocalRunner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("testdata/p7793/input.txt"));
+		Solution.main(new String[0]);
 	}
-
 }
