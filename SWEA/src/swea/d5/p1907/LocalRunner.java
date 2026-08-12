0a9489bc7fb5aefcd66341ee31ffc5f1af3e81ac
BaeHyungJoon <bhj01073206033@gmail.com>
2026-08-12 15:01:11 +0900

    SWEA p1907 모래성 쌓기

--- SWEA/src/swea/d5/p1907/LocalRunner.java
+++ SWEA/src/swea/d5/p1907/LocalRunner.java
@@ -1,10 +1,11 @@
 package swea.d5.p1907;
 
+import java.io.FileInputStream;
+
 public class LocalRunner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("testdata/p1907/input.txt"));
+		Solution.main(new String[0]);
 	}
-
 }
