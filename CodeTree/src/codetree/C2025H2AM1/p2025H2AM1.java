fb51b1dfa4a714276966dce86038990ab7f1a315
BaeHyungJoon <bhj01073206033@gmail.com>
2026-08-26 17:58:14 +0900

    배형준: P5648 원자 소멸 시뮬레이션

---
+++ CodeTree/src/codetree/C2025H2AM1/p2025H2AM1.java
@@ -0,0 +1,19 @@
+package codetree.C2025H2AM1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class p2025H2AM1 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+	}
+}
