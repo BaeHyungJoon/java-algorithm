c7bcb36445810265a43bb9fd00b4e20fef5946f5
BaeHyungJoon <bhj01073206033@gmail.com>
2026-09-02 00:53:11 +0900

    Programmers p42885 구명보트

---
+++ Programmers/src/programmers/lv2/p42860/LocalRunner.java
@@ -0,0 +1,10 @@
+package programmers.lv2.p42860;
+
+public class LocalRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Programmers/src/programmers/lv2/p42885/LocalRunner.java
@@ -0,0 +1,15 @@
+package programmers.lv2.p42885;
+
+public class LocalRunner {
+
+	public static void main(String[] args) {
+		
+		Solution solution = new Solution();
+		int[] people = {70, 50, 80, 50};
+		int limit = 100;
+		int result = solution.solution(people, limit);
+		System.out.println(result);
+		
+	}
+	
+}
