5a97a95c7d327ba07bee859ff6a9fc03124c38ab
BaeHyungJoon <bhj01073206033@gmail.com>
2026-09-02 14:37:22 +0900

    Programmers p42860 조이스틱

--- Programmers/src/programmers/lv2/p42860/LocalRunner.java
+++ Programmers/src/programmers/lv2/p42860/LocalRunner.java
@@ -3,8 +3,16 @@ package programmers.lv2.p42860;
 public class LocalRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Solution solution = new Solution();
+		String name1 = "JEROEN";
+		int result1 = solution.solution(name1);
+		System.out.println(result1);
+		
+		String name2 = "JAN";
+		int result2 = solution.solution(name2);
+		System.out.println(result2);
+		
 	}
 
 }
