3384782b8f6297b823556e43b615905ba3432ec3
BaeHyungJoon <bhj01073206033@gmail.com>
2026-09-10 17:29:23 +0900

    SWEA p1486 장훈이의 높은 선반 (완전탐색 + 재귀DFS + 백트래킹 구현)

--- SWEA/src/swea/d4/p1486/Solution.java
+++ SWEA/src/swea/d4/p1486/Solution.java
@@ -3,10 +3,12 @@ package swea.d4.p1486;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
+	
+	static int N, B, answer;
+	static int[] record;
 
 	public static void main(String[] args) throws IOException {
 
@@ -19,35 +21,20 @@ public class Solution {
 		for (int testCase = 1; testCase <= tc; testCase++) {
 
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); // 점원 수
-			int B = Integer.parseInt(st.nextToken()); // 선반 높이
+			N = Integer.parseInt(st.nextToken()); // 점원 수
+			B = Integer.parseInt(st.nextToken()); // 선반 높이
 
-			int[] record = new int[N]; // 점원들의 키 저장 int 배열
+			record = new int[N]; // 점원들의 키 저장 int 배열
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
 				record[i] = Integer.parseInt(st.nextToken()); // 점원들의 키 저장 완료
 			}
-
-			// 점원들의 키를 더했을 때 선반 높이보다는 커야하는 경우 중에 가장 낮은 탑의 높이를 만들어야 함.
-			// 일단 점원의 키 저장했을 때 이를 정렬해보자.
-			Arrays.sort(record);
-			int low = 0;
-			int high = N - 1;
-			int answer = 0;
-
-			while (low <= high) {
-				int mid = (low + high) / 2;
-				if (totalSum(record, high, mid) < B) {
-					high = mid - 1;
-					answer = totalSum(record, high, mid);
-				} else if(totalSum(record, high, mid) > B) {
-					low = mid + 1;
-					answer = totalSum(record, high, mid);
-				} else {
-					answer = 0;
-				}
-			}
-
+			
+			// 점원 수가 20명 이하이므로 점원이 선택될지, 안될지 2가지 경우의 수가 각각 있으니깐 2^20 = 약 100만개 경우의 수 존재
+			// 완전탐색 재귀 DFS 구현
+			
+			answer = Integer.MAX_VALUE;
+			dfs(0,0);
 			output.append('#').append(testCase).append(' ').append(answer).append('\n');
 
 		}
@@ -56,15 +43,20 @@ public class Solution {
 
 	}
 
-	static int totalSum(int[] record, int n, int mid) {
-
-		int sum = 0;
-		for (int i = mid - 1; i <= n - 1; i++) {
-			sum += record[i];
+	static void dfs(int index, int sum) {
+		if (sum >= B) {
+			answer = Math.min(answer, sum-B);
+			return;
 		}
 		
-
-		return sum;
+		if (index == N) {
+			return;
+		}
+		
+		dfs(index+1, sum + record[index]);
+		
+		dfs(index+1, sum);
+		
 	}
 
 }
