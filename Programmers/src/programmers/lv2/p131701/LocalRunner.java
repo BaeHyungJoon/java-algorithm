979cff7ac3e376a9f447b85f942c97a78fbb6f37
BaeHyungJoon <bhj01073206033@gmail.com>
2026-09-11 00:02:21 +0900

    Programmers p131701 연속 부분 수열 합의 개수

---
+++ Programmers/src/programmers/lv2/p131701/LocalRunner.java
@@ -0,0 +1,14 @@
+package programmers.lv2.p131701;
+
+public class LocalRunner {
+
+	public static void main(String[] args) {
+		
+		Solution solution = new Solution();
+		int[] elements = { 7, 9, 1, 1, 4 };
+		int result = solution.solution(elements);
+		System.out.println(result);
+	
+	}
+
+}
